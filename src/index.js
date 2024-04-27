c01a83240d12b6eeff7e23504f35120277719d91
andreslaguilavo <alaguilavo@poligran.edu.co>
2024-04-27 16:06:32 -0500

    Configuracion inicial de una Api en Express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('sdasd')
+import express from 'express'
+import 'dotenv/config'
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
