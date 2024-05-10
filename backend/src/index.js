b9a199acc2e72c0f92438f2b02eec1371ddd2364
andreslaguilavo <alaguilavo@poligran.edu.co>
2024-05-10 11:02:05 -0500

    Crear router de /cancion y metodo GET

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import 'dotenv/config'
+import routerCancion from './rutas/cancion.js'
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/cancion', routerCancion)
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
---
+++ backend/src/rutas/cancion.js
@@ -0,0 +1,34 @@
+import { Router } from 'express'
+import { prisma } from '../prisma.js'
+const router = Router()
+
+router.get('/', async (req, res) => {
+  const querys = req.query
+  const acceptedFormats = ['vinilo', 'MP3']
+  if (querys.formato && !acceptedFormats.includes(querys.formato)) {
+    res.send('Formato no válido')
+  }
+  const songs = await prisma.cancion.findMany({})
+
+  console.log(songs)
+  res.json({
+    songs
+  })
+})
+
+// router.post('/', async (req, res) => {
+//   const =  
+//   const querys = req.query
+//   const acceptedFormats = ['vinilo', 'MP3']
+//   if (querys.formato && !acceptedFormats.includes(querys.formato)) {
+//     res.send('Formato no válido')
+//   }
+//   const songs = await prisma.cancion.findMany({})
+
+//   console.log(songs)
+//   res.json({
+//     songs
+//   })
+// })
+
+export default router
