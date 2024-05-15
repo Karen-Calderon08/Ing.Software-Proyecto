033547841ea5a6a081aae63acea8ac50a7b276cc
andreslaguilavo <165935354+andreslaguilavo@users.noreply.github.com>
2024-05-15 10:57:34 -0500

    Agregar test daniel

--- backend/src/tests/andres.test.js
+++ backend/src/tests/andres.test.js
@@ -1,20 +1,6 @@
-// import sum from '../funciones/sum'
 import { test, expect } from '@jest/globals'
-import { validarFormatoCancion } from '../funciones/validarFormatoCancion.js'
 import { validarDatosUsuario } from '../funciones/validarDatosUsuario.js'
 
-test('Valida que el formato solicitado sea vinilo o mp3', () => {
-  expect(validarFormatoCancion('vinilo')).toBe(true)
-  expect(validarFormatoCancion('VINILO')).toBe(true)
-  expect(validarFormatoCancion('mp3')).toBe(true)
-  expect(validarFormatoCancion('MP3')).toBe(true)
-  expect(validarFormatoCancion(undefined)).toBe(false)
-  expect(validarFormatoCancion()).toBe(false)
-  expect(validarFormatoCancion('cualquiercosa')).toBe(false)
-  expect(validarFormatoCancion(null)).toBe(false)
-  expect(validarFormatoCancion(1234)).toBe(false)
-  expect(validarFormatoCancion(new Date())).toBe(false)
-})
 
 test('Valida que los datos del usuario sean correctos', () => {
   const usuarioValido = {
