477b8cc386cfe7afff244f4272a37741afbe4e74
andreslaguilavo <alaguilavo@poligran.edu.co>
2024-05-14 21:22:21 -0500

    Agregar validacion de formato en endpoint /

--- backend/src/rutas/cancion.js
+++ backend/src/rutas/cancion.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { prisma } from '../prisma.js'
+import { validarFormatoCancion } from '../funciones/validarFormatoCancion.js'
 const router = Router()
 
 router.get('/', async (req, res) => {
   const querys = req.query
-  const acceptedFormats = ['vinilo', 'MP3']
-  if (querys.formato && !acceptedFormats.includes(querys.formato)) {
+
+  if (querys.formato && !validarFormatoCancion(querys.formato)) {
     res.send('Formato no válido')
   }
   const songs = await prisma.cancion.findMany({})
