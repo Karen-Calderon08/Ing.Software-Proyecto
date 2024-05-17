ca3cc16e9e89dc9b7c71a6498c4ba78d13215674
andreslaguilavo <alaguilavo@poligran.edu.co>
2024-05-17 17:38:24 -0500

    Agregar endpoint de /cancion y /carrito-compras

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import 'dotenv/config'
 import routerCancion from './rutas/cancion.js'
+import routerCarritoCompras from './rutas/carritoCompras.js'
 const app = express()
 const port = process.env.PORT || 3000
-
+app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.use('/cancion', routerCancion)
+app.use('/carrito-compras', routerCarritoCompras)
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
---
+++ backend/src/rutas/carritoCompras.js
@@ -0,0 +1,36 @@
+import { Router } from 'express'
+import { prisma } from '../prisma.js'
+import z from 'zod'
+import { validarPropiedadesObjeto } from '../utilidades/validarPropiedadesObjeto.js'
+const router = Router()
+
+router.post('/agregar', async (req, res) => {
+  const body = req.body
+
+  const schema = {
+    usuarioId: z.number().positive(),
+    cancionId: z.number().positive(),
+    cantidad: z.number().positive(),
+    formato: z.union([z.literal('vinilo'), z.literal('mp3')])
+  }
+
+  try {
+    validarPropiedadesObjeto(body, schema)
+    await prisma.carrito.create({
+      data: {
+        usuarioId: body.usuarioId,
+        cancionId: body.cancionId,
+        cantidad: body.cantidad,
+        formato: body.formato
+      }
+    })
+    res.status(200).json({ message: 'Canción agregada al carrito' })
+  } catch (error) {
+    res.status(400).json({
+      message: 'Error al agregar la canción al carrito',
+      errors: error.data ?? error
+    })
+  }
+})
+
+export default router
