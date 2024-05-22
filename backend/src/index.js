accb733c81a9db873a8cbd38c1d519334c14ccd7
andreslaguilavo <alaguilavo@poligran.edu.co>
2024-05-22 17:16:16 -0500

    Agregar controlador y rutas para crear cliente

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import 'dotenv/config'
 import routerCancion from './rutas/cancion.js'
 import routerCarritoCompras from './rutas/carritoCompras.js'
 import routerPedido from './rutas/pedido.js'
+import routerCliente from './rutas/cliente.js'
 const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
@@ -10,6 +11,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use('/cliente', routerCliente)
 app.use('/cancion', routerCancion)
 app.use('/carrito-compras', routerCarritoCompras)
 app.use('/pedidos', routerPedido)
---
+++ backend/src/schemas.js
@@ -0,0 +1,22 @@
+import { z } from 'zod'
+
+export const clienteSchema = {
+  nombre: z
+    .string()
+    .min(1, 'El nombre es requerido.')
+    .max(50, 'El nombre es demasiado largo.'),
+  apellido: z
+    .string()
+    .min(1, 'El apellido es requerido.')
+    .max(50, 'El apellido es demasiado largo.'),
+  correo: z.string().email('El correo no es válido.'),
+  contrasena: z
+    .string()
+    .min(8, 'La contraseña debe tener al menos 8 caracteres.'),
+  direccion: z.optional(
+    z
+      .string()
+      .min(8, 'La dirección debe tener al menos 8 digitos.')
+      .max(50, 'La dirección es demasiado larga.')
+  )
+}
