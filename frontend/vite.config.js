ce9f9d1eba66a2dff4b467e9f26a4c4721fabd10
andreslaguilavo <alaguilavo@poligran.edu.co>
2024-05-25 20:17:53 -0500

    Añadir configuracion para vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,32 @@ export default {
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: ['./cart.html', './index.html']
+      input: [
+        './cart.html',
+        './detail.html',
+        './index.html',
+        './perfil.html',
+        './shop.html',
+        './signin.html',
+        './signup.html',
+        './subir.html',
+
+        './js/cart/index.js',
+        './js/detail/index.js',
+        './js/index/index.js',
+        './js/perfil/index.js',
+        './js/shop/index.js',
+        './js/signup/index.js',
+        './js/signup/index.js',
+        './js/subir/index.js',
+        './js/main.js',
+        './js/carritoCompras.js',
+        './js/obtenerProductos.js',
+        './js/obtenerUsuarioActivo.js',
+        './js/obtenerProductos.js',
+        './js/detail/agregarAlCarrito.js',
+        './js/detail/escogerFormato.js'
+      ]
     }
   }
 }
