0818c319ff3953af7c0cf4a41223a664168d2124
andreslaguilavo <alaguilavo@poligran.edu.co>
2024-05-25 20:17:43 -0500

    Añadir endpoint para manejar el auth

--- backend/src/controlador/auth.controlador.js
+++ backend/src/controlador/auth.controlador.js
@@ -5,16 +5,33 @@ import { validarPropiedadesObjeto } from '../utilidades/validarPropiedadesObjeto
 import jwt from 'jsonwebtoken'
 
 export class AuthControlador {
+  /**
+   * @typedef {Object} TokenResponse
+   * @property {string} token - El token JWT generado.
+   */
+
+  /**
+   * Valida las credenciales del usuario y genera un token JWT si las credenciales son válidas.
+   *
+   * @param {Object} req - El objeto de solicitud HTTP.
+   * @param {Object} req.body - El cuerpo de la solicitud, que debe contener las credenciales del usuario.
+   * @param {string} req.body.correo - El correo electrónico del usuario.
+   * @param {string} req.body.contrasena - La contraseña del usuario.
+   * @param {Object} res - El objeto de respuesta HTTP.
+   * @returns {Promise<TokenResponse>} Una promesa que se resuelve cuando se ha completado el procesamiento de la solicitud.
+   * @throws {Error} Lanza un error si las credenciales del usuario son inválidas.
+   */
   static async validarUsuario(req, res) {
     const body = req.body
     try {
       const isValidSchema = validarPropiedadesObjeto(body, authSchema)
-
+      console.log(isValidSchema)
       if (!isValidSchema.isValid) {
         return res.status(400).json({ mensaje: isValidSchema })
       }
 
       const cliente = await ClienteModelo.obtenerCliente(body.correo)
+      console.log(cliente)
       const { contrasena } = body
       console.log(contrasena, cliente.contrasena)
       const isValidPassword = await validarContraseña(
