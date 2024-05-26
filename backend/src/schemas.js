450fbb43916e8c62f298e3f08ffb3ff8c5ff97fa
Dschacon <dschacon@poligran.edu.co>
2024-05-25 20:16:05 -0500

    Añadir logica para el manejo de las canciones

--- backend/src/schemas.js
+++ backend/src/schemas.js
@@ -18,10 +18,39 @@ export const clienteSchema = {
       .string()
       .min(8, 'La dirección debe tener al menos 8 digitos.')
       .max(50, 'La dirección es demasiado larga.')
-  )
+  ),
+  tipoUsuario: z.string()
 }
 
 export const authSchema = {
   correo: z.string().email('El correo no es válido.'),
-  contrasena: z.string().min(8, 'La contraseña debe tener al menos 8 caracteres.')
+  contrasena: z
+    .string()
+    .min(8, 'La contraseña debe tener al menos 8 caracteres.')
+}
+
+export const cancionSchema = {
+  nombre: z
+    .string()
+    .min(1, 'El nombre es requerido.')
+    .max(50, 'El nombre es demasiado largo.'),
+  artista: z
+    .string()
+    .min(1, 'El artista es requerido.')
+    .max(50, 'El artista es demasiado largo.'),
+  duracion: z.optional(z.number().min(1, 'La duración debe ser mayor a 0.')),
+  caratula: z.string().url('La caratula debe ser una URL válida.'),
+  mp3: z.optional(
+    z.object({
+      tamano: z.string().min(1, 'El tamaño debe ser mayor a 0.'),
+      calidad: z.string().min(1, 'La calidad es requerida.'),
+      precio: z.number().min(1, 'El precio debe ser mayor a 0.')
+    })
+  ),
+  vinilo: z.optional(
+    z.object({
+      stock: z.number().min(1, 'El stock debe ser mayor a 0.'),
+      precio: z.number().min(1, 'El precio debe ser mayor a 0.')
+    })
+  )
 }
